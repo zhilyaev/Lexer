08ab8cdbed758229865fdc67661cd1884252b6cc
Дмитрий Жиляев <Dmitriy@zhilyaev.net>
2017-03-25 06:06:19 +0300

    Begin...

---
+++ src/diamon/spuusk/Lexer.java
@@ -0,0 +1,9 @@
+package diamon.spuusk;
+
+import java.util.HashMap;
+
+public class Lexer {
+    Lexer (String input){
+
+    }
+}
---
+++ src/diamon/spuusk/Main.java
@@ -0,0 +1,11 @@
+package diamon.spuusk;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(TokenMap.TokenEnum.NUMBER);
+    }
+
+
+}
